a79f3a8e121fa32630aca271da5fd48c39c7d97e
AstronautSion <astronaut.sion@gmail.com>
2021-01-12 01:48:47 +0900

    basic typing

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # MySendBox
+
 javascript 이것저것 개발
+
+
+### [HTML]
+
+~~~html
+    <div class="typing"></div>
+~~~
+<br/>
+
+### [ JS ] Call the Typing
+
+~~~js
+
+new Typing({
+    target : '.typing', 
+    text: 'Excepteur ad veniam magna eu commodo fugiat culpa duis officia ea minim.'
+
+}, function(target,text){ //callback
+    console.log(target, text);
+});
+
+~~~
+ 
+
+### [ CSS ]
+
+~~~css
+.is-typing{
+    position:relative;
+}
+.is-typing:after{
+    position:absolute; 
+    content:'_'; 
+    display:inline; 
+    opacity:1; 
+    animation: typing .5s .5s ease-in Infinite Alternate;
+}
+@keyframes typing{ 
+    from {opacity:0;} 
+    to {opacity:1;}
+}
+
+~~~
---
+++ typing/typing.js
@@ -0,0 +1,60 @@
+class Typing{
+    constructor(option, callback){
+        this.target = null;
+        this.text = null;
+        this.callback = null;
+        this.textKey = null;
+        this.setTime = null;
+        this.splitTime = null;
+        this.initEvent(option, callback);
+        this.typingEvent();
+    }
+
+    initEvent(option, callback){
+        this.target = document.querySelector(option.target);
+        this.text = this.escape(option.text);
+        this.callback = callback;
+
+        this.aniTime = 0;
+        this.textKey = 0;
+        this.textSplitPoint = 1;
+        this.setTime = this.text.length * 2;
+        this.splitTime = Math.floor( this.setTime / this.text.length );
+    }
+    typingEvent(){
+        this.target.classList.add('is-typing');
+        let typingAni = () =>{
+            this.aniTime++;
+            if(this.textSplitPoint == this.aniTime){ //event 
+                this.textSplitPoint += this.splitTime;
+                this.target.insertAdjacentHTML('beforeend',this.text[this.textKey]);
+                this.textKey++;
+            }
+
+            if(this.aniTime < this.setTime){ // loop
+                requestAnimationFrame(typingAni);
+
+            }else if(this.aniTime  == this.setTime){  //end
+                cancelAnimationFrame(typingAni);
+                this.afterTypingEvent();
+            }
+        };
+        requestAnimationFrame(typingAni);
+    }
+    afterTypingEvent(){
+        if(this.callback){
+            this.callback(this.target, this.text);
+        }
+    }
+    escape(unsafe){
+        return unsafe
+             .replace(/&/g, "&amp;")
+             .replace(/</g, "&lt;")
+             .replace(/>/g, "&gt;")
+             .replace(/"/g, "&quot;")
+             .replace(/'/g, "&039;");
+    }
+    
+}
+
+ 
